3437dcb44381d66a33a092e474e95e1a75339805
Zhang Rui <zrui16@hotmail.com>
2017-01-04 23:23:37 +0800

    Simplify gauss.lua

--- gauss.lua
+++ gauss.lua
@@ -3,13 +3,10 @@ local f = require 'mp.f'
 local N = 15000
 f.precision = math.ceil(N/math.log(2,10))
 
-local a = f(1)
-local b = f(1/2); b:sqrt(b) -- b = 1/sqrt(2)
-local t = f(1/4)
-local p = f(1, nil, 0) -- p does not need much precision
+local a, b, t = f(1), f():sqrt(.5), f(.25)
 local tmp = f()
 
-for i = 1,16 do
+for i = 0, 15 do
 	tmp: add(a, b)
 	tmp: div_2exp(tmp, 1)
 	b: mul(a, b)
@@ -17,9 +14,8 @@ for i = 1,16 do
 	a: swap(tmp) -- tmp is now old a
 	tmp: sub(tmp, a)
 	tmp: mul(tmp, tmp)
-	tmp: mul(tmp, p)
+	tmp: mul_2exp(tmp, i)
 	t: sub(t, tmp)
-	p: mul_2exp(p, 1)
 end
 
 tmp: mul(a, a)
